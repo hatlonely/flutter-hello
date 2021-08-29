8d3901634fab493eca3ae354f8a9f6a76f10c334
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-08-29 21:29:31 +0800

    update

--- flutter_hello/lib/main.dart
+++ flutter_hello/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hello/buildin/text.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Hello Flutter'),
+      home: TextDemo(),
     );
   }
 }
