21097299822b23a5a5672929c7bd8ea590cace97
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-08-30 00:17:25 +0800

    update

--- flutter_hello/lib/buildin/icon.dart
+++ flutter_hello/lib/buildin/icon.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:simple_icons/simple_icons.dart';
 
 // https://fonts.google.com/icons?selected=Material+Icons
 // https://fontawesome.com/v6.0/icons?d=gallery&p=2&s=solid
+// https://icons8.com/line-awesome
+// https://simpleicons.org/
 class IconDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -21,6 +25,27 @@ class IconDemo extends StatelessWidget {
         FaIcon(FontAwesomeIcons.thumbsUp),
         FaIcon(FontAwesomeIcons.search),
         FaIcon(FontAwesomeIcons.gamepad),
+        Icon(LineAwesomeIcons.twitter),
+        Icon(LineAwesomeIcons.facebook),
+        Icon(LineAwesomeIcons.what_s_app),
+        Icon(SimpleIcons.github),
+        Icon(SimpleIcons.redis),
+        Icon(SimpleIcons.kubernetes),
+        Icon(SimpleIcons.mongodb),
+        Icon(SimpleIcons.mysql),
+        Icon(SimpleIcons.prometheus),
+        Icon(SimpleIcons.elasticsearch),
+        Icon(SimpleIcons.kibana),
+        Icon(SimpleIcons.influxdb),
+        Icon(SimpleIcons.grafana),
+        Icon(SimpleIcons.wechat),
+        Icon(SimpleIcons.alibabacloud),
+        Icon(SimpleIcons.alibabadotcom),
+        Icon(SimpleIcons.huawei),
+        Icon(SimpleIcons.baidu),
+        Icon(SimpleIcons.google),
+        Icon(SimpleIcons.apple),
+        Icon(SimpleIcons.googlechrome),
       ],
     );
   }
