2fc9ff65ad5064242733efb9c15604bcc22d59bb
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-09-01 00:20:14 +0800

    update

--- flutter_hello/lib/buildin/scaffold.dart
+++ flutter_hello/lib/buildin/scaffold.dart
@@ -7,11 +7,39 @@ class ScaffoldDemo extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Sample Code'),
       ),
-      body: Center(child: Text('text')),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {print("click")},
         tooltip: 'btn',
         child: const Icon(Icons.add),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text('We move under cover and we move as one'),
+            const Text('Through the night, we have one shot to live another day'),
+            const Text('We cannot let a stray gunshot give us away'),
+            const Text('We will fight up close, seize the moment and stay in it'),
+            const Text('It’s either that or meet the business end of a bayonet'),
+            const Text('The code word is ‘Rochambeau,’ dig me?'),
+            Text('Rochambeau!', style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0)),
+            Row(
+              children: [
+                Expanded(
+                  child: Text('Deliver features faster', textAlign: TextAlign.center),
+                ),
+                Expanded(
+                  child: Text('Craft beautiful UIs', textAlign: TextAlign.center),
+                ),
+                Expanded(
+                  child: FittedBox(
+                    fit: BoxFit.contain, // otherwise the logo will be tiny
+                    child: FlutterLogo(),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
